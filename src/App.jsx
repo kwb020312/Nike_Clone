2de9f48359334e6af035da48d4ca24ce3c8780d4
김우빈 <kwb020312@naver.com>
2023-08-20 22:10:23 +0900

    Feat: prepare assets & apply base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-function App() {
-  return <>HelloWorld</>;
-}
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    <section className="padding">인기품목</section>
+    <section className="padding">한정판</section>
+    <section className="padding-x py-10">서비스</section>
+    <section className="padding">특별가</section>
+    <section className="bg-pale-blue padding">고객리뷰</section>
+    <section className="padding-x sm:py-32 py-16 w-full">구독</section>
+    <section className="bg-black padding-x padding-t pb-8">정보</section>
+  </main>
+);
 
 export default App;
