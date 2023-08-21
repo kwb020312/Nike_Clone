91bf1bf97bcc3e62eab92bb892190cb5e36f9cfb
김우빈 <kwb020312@naver.com>
2023-08-21 21:14:40 +0900

    Feat: NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Nav from "./components/Nav";
 import {
   CustomerReviews,
   Footer,
@@ -11,7 +12,7 @@ import {
 
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero />
     </section>
