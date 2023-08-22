797cf89a8f23e4b2c17ac8d4edb8658b679477cf
김우빈 <kwb020312@naver.com>
2023-08-22 21:46:44 +0900

    Feat: 인기있는 상품 탭

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,5 +1,26 @@
+import PopularProductCard from "../components/PopularProductCard";
+import { products } from "../constants";
+
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">
+          <span className="text-coral-red">인기</span>상품
+        </h2>
+        <p className="lg:max-w-lg mt-2 text-slate-gray">
+          인기 있는 컬렉션으로 최고의 품질과 스타일을 경험해 보세요. 편안함,
+          디자인, 가치 모두 갖추었습니다.
+        </p>
+      </div>
+
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default PopularProducts;
