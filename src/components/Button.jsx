43a7bc33ec1836854e6e5e825dcc18c01e0c8b6c
김우빈 <kwb020312@naver.com>
2023-08-23 22:37:28 +0900

    Feat: Nike 구독 탭

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,10 +4,13 @@ const Button = ({
   backgroundColor,
   textColor,
   borderColor,
+  fullWidth,
 }) => {
   return (
     <button
       className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full ${
+        fullWidth && "w-full"
+      } ${
         backgroundColor
           ? `${backgroundColor} ${textColor} ${borderColor}`
           : "bg-coral-red text-white border-coral-red"
